0b698b0c7e4ade6c195d509ed3c5f49689bf8587
Rohit Mondal <mondalrohit903@gmail.com>
2024-03-04 12:38:57 +0530

    Frontend

--- my-react-app/src/App.tsx
+++ my-react-app/src/App.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
 import './App.css'
+import { CodingPage } from './components/CodingPage'
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Landing } from './components/Landing';
 
 function App() {
   return (
