498ad74150a84b0365c715954254e81e2042ae39
Rohit Mondal <mondalrohit903@gmail.com>
2024-03-05 18:56:46 +0530

    Frontend

---
+++ my-react-app/src/assets/index.js
@@ -0,0 +1,44 @@
+//Tech
+
+import codecademy from './tech/codecademy.png'
+import htmlIcon from './tech/html.png'
+import cssIcon from './tech/css.png'
+import jsIcon from './tech/javascript.png'
+import reactIcon from './tech/reactjs.png'
+import threeIcon from './tech/threejs.svg'
+import tailwindIcon from './tech/tailwind.png'
+import gitIcon from './tech/git.png'
+import githubIcon from './tech/github.png'
+import figmaIcon from './tech/figma.png'
+import reduxIcon from './tech/redux.png'
+import gsapIcon from './tech/gsap.png'
+import shopifyIcon from './tech/shopify.png'
+import framerIcon from './tech/framer.png'
+import webflowIcon from './tech/webflow.svg'
+
+//Memoji
+
+import memojiHand from './memoji/memoji_hand-min.png'
+import memojiHello from './memoji/memoji_hello-min.png'
+import memojiOk from './memoji/memoji_ok-min.png' 
+
+export {
+    codecademy,
+    htmlIcon,
+    cssIcon,
+    jsIcon,
+    reactIcon,
+    threeIcon,
+    tailwindIcon,
+    gitIcon,
+    githubIcon,
+    figmaIcon,
+    reduxIcon,
+    gsapIcon,
+    shopifyIcon,
+    framerIcon,
+    webflowIcon,
+    memojiHand,
+    memojiHello,
+    memojiOk,
+}
