9e8e1b43653df254efb094c449a94f321925cd3e
Rohit Mondal <mondalrohit903@gmail.com>
2024-03-06 18:10:57 +0530

    Frontend

--- my-react-app/src/components/nav/NavLinks.tsx
+++ my-react-app/src/components/nav/NavLinks.tsx
@@ -13,6 +13,15 @@ const NavLinks = ({ handleClick }) => {
             About
           </a>
         </li>
+        <li className="border-t-[1px] border-grayscale-950 w-full flex justify-center p-3">
+          <a
+            className="active:text-grayscale-100"
+            onClick={handleClick}
+            href="#ide"
+          >
+            IDE
+          </a>
+        </li>
         <li className="border-t-[1px] border-b-[1px] border-grayscale-950 w-full flex justify-center p-3">
           <a
             className="active:text-grayscale-100"
